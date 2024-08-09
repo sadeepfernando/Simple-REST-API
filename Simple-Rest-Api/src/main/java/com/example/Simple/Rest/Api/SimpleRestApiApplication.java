48892851c1cb14f6594f3af75d85f98def84df06
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-08-09 12:41:32 +0530

    first commit

---
+++ Simple-Rest-Api/src/main/java/com/example/Simple/Rest/Api/SimpleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simple.Rest.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApiApplication.class, args);
+	}
+
+}
